2899d8ede071daa9c4b5a05734e8849af612b622
AndreyKaOCoder <147846821+AndreyKaOCoder@users.noreply.github.com>
2023-11-12 00:34:51 +0300

    :sparkles: Create file builder.py
    Create file in keyboards -> builder

---
+++ keyboards/builder.py
@@ -0,0 +1,10 @@
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from handlers.inventorization_commands import inv_book
+
+def kb_inventory():
+    builder = ReplyKeyboardBuilder()
+    for item in inv_book.sheetnames:
+        builder.button(text = item)
+    builder.adjust(3)
+    builder.button(text = "💻 Главное меню")
+    return builder.as_markup(resize_keyboard = True)
