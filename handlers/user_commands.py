be487af35f67027635b025789d609f0a15d11525
AndreyKaOCoder <147846821+AndreyKaOCoder@users.noreply.github.com>
2023-11-13 21:50:17 +0300

    :art: Update user_commands.py
    Update text in file user_commands

--- handlers/user_commands.py
+++ handlers/user_commands.py
@@ -1,19 +1,27 @@
 from aiogram import Router, F
 from aiogram.types import Message
 from aiogram.filters import CommandStart
+from aiogram.fsm.context import FSMContext
 
 from keyboards import reply, builder
+from utils.states import Search
 
 router = Router()
 
 @router.message(CommandStart())
-async def start (message: Message):
+async def comm_start (message: Message):
     await message.answer(f"💻 Бот - Система инвентаризации", reply_markup = reply.main)
 
 @router.message(F.text.lower() == "🧾 инвентаризация")
-async def inventorization (message: Message):
+async def comm_inventorization (message: Message):
     await message.answer(f"👆 Выберите инвентаризацию:", reply_markup = builder.kb_inventory())
 
 @router.message(F.text.lower() == "💻 главное меню")
-async def start (message: Message):
+async def commm_start (message: Message, state: FSMContext):
+    await state.clear()
     await message.answer(f"💻 Бот - Система инвентаризации", reply_markup = reply.main)
+
+@router.message(F.text.lower() == "🔎 поиск")
+async def comm_search (message: Message, state: FSMContext):
+    await state.set_state(Search.index)
+    await message.answer(f"🖊 Введите оборудование, которое хотите найти: (серийный номер/инвентарный/название)", reply_markup = reply.exit)
