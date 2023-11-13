5fe0cc5cf52915b932746281d492b094fc3d556f
AndreyKaOCoder <147846821+AndreyKaOCoder@users.noreply.github.com>
2023-11-13 21:49:44 +0300

    :art: Update inventarization_commands.py
    Update text in file inventarization_commands

--- handlers/inventorization_commands.py
+++ handlers/inventorization_commands.py
@@ -4,26 +4,28 @@ import asyncio
 from aiogram import Router, F
 from aiogram.types import Message
 from aiogram.filters import Command, CommandStart
+from aiogram.fsm.context import FSMContext
 
 from keyboards import reply, inline
+from utils.states import Search
 
 router = Router()
 inv_book = openpyxl.load_workbook(filename = "data/Inventarization.xlsx", data_only = True)
 
 for item in inv_book.sheetnames:
     @router.message(F.text == item)
-    async def inventarization (message: Message):
+    async def comm_inventarization (message: Message):
         sheet = inv_book[message.text]
         text = ""
-        for i in range(sheet.min_row, sheet.max_row):
+        for i in range(sheet.min_row, sheet.max_row + 1):
             text += f"🔢 № {sheet['A' + str(i)].value}\n📅 Дата ввода: {sheet['B' + str(i)].value}\n✏️ Сер. номер: {sheet['C' + str(i)].value}\n📌 Инв. номер: {sheet['D' + str(i)].value}\n📝 Наименование: {sheet['E' + str(i)].value}\n🚪 Местоположение: {sheet['F' + str(i)].value}\n\n"
         for x in range(0, len(text), 4096):
             mess = text[x: x + 4096]   
             await asyncio.sleep(0.5)      
             await message.answer(mess)
 
-@router.message(F.text.lower())
-async def search (message: Message):
+@router.message(Search.index)
+async def comm_search (message: Message, state: FSMContext):
     mess_text = False
     for work_sheet in inv_book.worksheets:
         for row in work_sheet.iter_rows(min_row=1, max_row=work_sheet.max_row, min_col=1, max_col=work_sheet.max_column):
@@ -36,6 +38,22 @@ async def search (message: Message):
                     await message.answer(text)
     if mess_text == False:
         await message.answer("❌ Данного оборудования не найдено!")
+
+@router.message(F.text.lower() == "📊 общая статистика")
+async def comm_stats (message: Message):
+    countsb = 0
+    countmon = 0
+    for work_sheet in inv_book.worksheets:  
+        for row in work_sheet.iter_rows(min_row=1, max_row=work_sheet.max_row):
+            for cell in row:
+                if cell.value == "Системный блок": 
+                    countsb += 1   
+                elif cell.value == "Монитор": 
+                    countmon += 1  
+    await message.answer(f"📊 Общая статистика\n\n🖥 Кол-во мониторов: {countsb} шт.\n💻 Кол-во системных блоков: {countmon} шт.")
+                    
+                
+
   
 
 
